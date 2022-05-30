e1b6405dde1cf19dfd168969fcc75c9004c8f63e
Sietze Teunissen <sietze1983@hotmail.com>
2022-05-30 16:46:24 +0200

    tandartspraktijk

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom';
 import TopMenu from './components/TopMenu';
 import HomePage from './pages/Home';
 import CavitiesPage from './pages/Cavities';
@@ -6,16 +11,27 @@ import AppointmentsPage from './pages/Appointments';
 import WhiteningPage from './pages/Whitening';
 import './App.css';
 
+
 function App() {
-  return (
-    <>
-      <TopMenu />
-      <HomePage />
-      <CavitiesPage />
-      <AppointmentsPage />
-      <WhiteningPage />
-    </>
-  );
+    return (
+        <Router>
+            <TopMenu />
+            <Switch>
+                <Route exact path="/">
+                    <HomePage />
+                </Route>
+                <Route path="/gaatjes">
+                    <CavitiesPage />
+                </Route>
+                <Route path="/afspraak-maken">
+                    <AppointmentsPage />
+                </Route>
+                <Route path="/tanden-bleken">
+                    <WhiteningPage />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
